7a32eaf24a06e00f5b088806a58e5cde2a3a6ab9
Fama Roman <famaroman@gmail.com>
2023-08-22 17:48:57 -0300

    final version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 
+import compressor from "astro-compressor";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  srcDir: "./src",
+  publicDir: "./public",
   server: {
-    port: 10000,
+    port: 10000
   },
   adapter: node({
     mode: "standalone"
-  })
+  }),
+  integrations: [compressor({
+    gzip: true,
+    brotli: true
+  })]
 });
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,15 @@
 import { load } from "cheerio"
 
+export const listMetadataToGet = [
+    ["property", "og:title"],
+    ["property", "og:description"],
+    ["name", "description"],
+    ["property", "og:image"],
+    ["property", "og:image:alt"],
+    ["property", "og:url"],
+    ["property", "og:type"]
+]
+
 export function doubleZero(number) {
     if (number < 10) return "0" + number
     return number
@@ -14,11 +24,18 @@ export function secondsToDDHHMMSS(number) {
     return `${doubleZero(day)}d ${doubleZero(hours)}:${doubleZero(min)}:${doubleZero(secs)}`
 }
 
-export async function getDataMeta(url, arrayPropertys) {
+export async function getDataMeta(url) {
+
     let response = await fetch(url)
     response = await response.text()
     const $ = load(response)
-    return arrayPropertys.map((item)=>{
-        return $(`meta[${item[0]}="${item[1]}"]`).attr("content")
-    })
+    return listMetadataToGet
+        .map((item) => {
+            let content = $(`meta[${item[0]}="${item[1]}"]`).attr("content")
+            if (content !== undefined) {
+                return [item[0], item[1], content]
+            }
+            return null
+        })
+        .filter((item) => item !== null)
 }
